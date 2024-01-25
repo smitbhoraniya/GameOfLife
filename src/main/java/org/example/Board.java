d1b6506a688e25d019c91021422d58cd38214d73
smit.bhoraniya_ftc <smit.bhoraniya_ftc@external.swiggy.in>
2024-01-25 13:06:41 +0530

    Implemented the seeds population logic

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -3,13 +3,40 @@ package org.example;
 import org.example.cell.Cell;
 import org.example.cell.CellX;
 
-import java.util.ArrayList;
+import java.util.Random;
 
 public class Board {
     private Cell[][] board;
+    private final int seeds;
 
-    public Board(int row, int column) {
+    public Board(int row, int column, int seeds) {
+        if (row < 0 || column < 0) {
+            throw new IllegalArgumentException("Board should have positive numbers of cells.");
+        }
+        if (seeds < 0) {
+            throw new IllegalArgumentException("Seeds should be positive.");
+        }
+        if (row * column < seeds) {
+            throw new IllegalArgumentException("Seeds should not exceed the total cells.");
+        }
         this.board = new Cell[row][column];
+        this.seeds = seeds;
+        initBoard();
+    }
+
+    private void initBoard() {
+        int populatedSeed = 0;
+
+        Random random = new Random();
+        while (populatedSeed != this.seeds) {
+            int row = random.nextInt(0, board.length);
+            int column = random.nextInt(0, board[0].length);
+
+            if (board[row][column] == null) {
+                board[row][column] = new CellX();
+                populatedSeed++;
+            }
+        }
     }
 
     public int getEmptyCells() {
@@ -24,4 +51,6 @@ public class Board {
 
         return count;
     }
+
+
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -12,15 +12,58 @@ public class BoardTest {
 
     @Test
     public void validBoard() {
-        assertDoesNotThrow(() -> new Board(0, 0));
+        assertDoesNotThrow(() -> new Board(0, 0, 0));
     }
 
     @Test
+    public void NegativeRow() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Board(-1, 0, 0));
+
+        String actual = exception.getMessage();
+        String expected = "Board should have positive numbers of cells.";
+    }
+
+    @Test
+    public void negativeColumn() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Board(0, -1, 0));
+
+        String actual = exception.getMessage();
+        String expected = "Board should have positive numbers of cells.";
+    }
+
+    @Test
+    public void negativeSeeds() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Board(0, 0, -1));
+
+        String actual = exception.getMessage();
+        String expected = "Board should have positive numbers of cells.";
+    }
+
+    @Test
+    public void seedsExceedTotalCells() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new Board(3, 3, 10));
+
+        String actual = exception.getMessage();
+        String expected = "Board should have positive numbers of cells.";
+    }
+
+
+    @Test
     public void getFreeCells() {
-        Board board = new Board(3, 3);
+        Board board = new Board(3, 3, 0);
 
         int actual = board.getEmptyCells();
         int expected = 9;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getFreeCellsWithSeeds() {
+        Board board = new Board(3, 3, 2);
+
+        int actual = board.getEmptyCells();
+        int expected = 7;
 
         assertEquals(expected, actual);
     }
